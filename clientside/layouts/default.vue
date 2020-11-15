61a151134fd0b250cb0b757238d8ce3564b11079
Theo Iyonor <symplytheo@gmail.com>
2020-11-15 13:11:39 +0100

    Initial commit

---
+++ clientside/layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app dark>
+    <v-app-bar fixed app dark color="primary">
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <nuxt />
+        <Snackbar />
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <span>&copy; {{ new Date().getFullYear() }}</span>
+      <span class="pl-1">{{ title }}</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: 'Music Manager',
+    }
+  },
+}
+</script>
---
+++ serverside/config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb://localhost:27017/musicmanager',
+  secret: '$Chegbe@2020'
+}
